14ba0acc1eb15484dfc974c22a49e06e8cf95037
Mauro Werder <mauro3@runbox.com>
2024-06-11 14:01:59 +0200

    T3 done

--- melt.jl
+++ melt.jl
@@ -5,5 +5,25 @@ function melt(T, melt_factor)
         return 0.0
     end
 end
-
+# try it out
 melt(1, 0.5)
+
+
+function accumulate(T, P, T_threshold)
+    if T <= T_threshold
+        P
+    else
+        return 0.0
+    end
+end
+# try it out
+accumulate(0, 5, 0.5)
+accumulate(5, 5, 7)
+
+
+function lapse(T, dz, lapse_rate)
+    return T + dz * lapse_rate
+end
+# try it out
+lapse(5, 100, 1)
+lapse(5, -100, 1)
