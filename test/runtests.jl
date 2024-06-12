21765f3556b658b22913b03209fe44bee75f7b0f
Mauro Werder <mauro3@runbox.com>
2024-06-12 16:26:13 +0200

    T7-v2: using Test-package instead of asserts

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,23 @@
 include("../src/GlacierMassBalanceModel.jl")
 
+# This is Julia's standard testing environment
+using Test
+
 # test melt
-@assert melt(0, 1) == 0
-@assert melt(-10, 1) == 0
-@assert melt(1, 1) == 1
-@assert melt(4, 7) == 4*7
-@assert melt(4, 0.01) == 4*0.01
+@test melt(0, 1) == 0
+@test melt(-10, 1) == 0
+@test melt(1, 1) == 1
+@test melt(4, 7) == 4*7
+@test melt(4, 0.01) == 4*0.01
 
 # test accumulate
-@assert accumulate(0, 5, 4) > 0
-@assert accumulate(5, 5, 4) == 0
+@test accumulate(0, 5, 4) > 0
+@test accumulate(5, 5, 4) == 0
 
 # test lapse
-@assert lapse(5, 100, 1) > 5
-@assert lapse(5, -100, 1) < 5
+@test lapse(5, 100, 1) > 5
+@test lapse(5, -100, 1) < 5
 
 # integration test of simple.jl
 include("../examples/simple.jl")
-@assert glacier_net_balance ≈ -0.11096645178976892
+@test glacier_net_balance ≈ -0.11096645178976892
